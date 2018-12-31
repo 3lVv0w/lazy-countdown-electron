f4247aa9ca97241ac295e0945f5be89df1030783
3lVv0w <krseisenh@gmail.com>
2018-12-31 21:34:15 +0700

    Initial commit

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false,
+    titleBarStyle: 'hidden'
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
