c6868fe9f8cc99abe7ea6779a294ae1a9030b8fc
3lVv0w <krseisenh@gmail.com>
2020-02-02 12:52:12 +0700

    wait for 2021

--- javascript/main.js
+++ javascript/main.js
@@ -1,74 +1,77 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Loaded')
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded");
 
-  var container = document.querySelector('.container')
-  var countdown = document.querySelector('#countdown')
-  var newYear = document.querySelector('#newyear')
-  var newYearText = document.querySelector('#newYearText')
+  var container = document.querySelector(".container");
+  var countdown = document.querySelector("#countdown");
+  var newYear = document.querySelector("#newyear");
+  var newYearText = document.querySelector("#newYearText");
 
-  var NEW_YEAR = 15462756e5
+  var NEW_YEAR = 15778116e5;
 
-  var isResumed = false
-  var isNewYear = false
+  var isNewYear = false;
 
-
-  document.querySelector('body').addEventListener('click', function () {
+  document.querySelector("body").addEventListener("click", function() {
     if (isNewYear) {
-      var audioEl = document.querySelector('audio')
+      var audioEl = document.querySelector("audio");
 
       if (audioEl) {
-        audioEl.play()
+        audioEl.play();
       } else {
-        window.createAudio()
+        window.createAudio();
       }
     }
-  })
+  });
 
-  window.createAudio = function () {
-    var audio = document.createElement('audio')
+  window.createAudio = function() {
+    var audio = document.createElement("audio");
 
-    audio.src = './assets/media/newyear.mp3'
-    audio.play()
+    audio.src = "./assets/media/newyear.mp3";
+    audio.play();
 
-    container.appendChild(audio)
-  }
+    container.appendChild(audio);
+  };
 
-  window.itsNewYear = function () {
-    isNewYear = true
+  window.itsNewYear = function() {
+    isNewYear = true;
 
-    countdown.style.display = 'none'
+    countdown.style.display = "none";
 
-    newYear.style.display = 'block'
+    newYear.style.display = "block";
 
-    newYearText.style.display = 'block'
-    createAudio()
-  }
+    newYearText.style.display = "block";
+    createAudio();
+  };
 
-  window.timer = setInterval(function () {
-    var ms = NEW_YEAR - Date.now()
-    var secs = Math.floor(ms / 1000)
+  window.timer = setInterval(function() {
+    var ms = NEW_YEAR - Date.now();
+    var secs = Math.floor(ms / 1000);
 
     var hours = Math.floor(secs / 3600);
-    var minutes = Math.floor((secs - (hours * 3600)) / 60);
-    var seconds = secs - (hours * 3600) - (minutes * 60);
+    var minutes = Math.floor((secs - hours * 3600) / 60);
+    var seconds = secs - hours * 3600 - minutes * 60;
 
-    document.querySelector('#countdown').innerHTML = hours.toFixed(0).padStart(2, '0') + ":" + minutes.toFixed(0).padStart(2, '0') + ":" + seconds.toFixed(0).padStart(2, '0');
+    document.querySelector("#countdown").innerHTML =
+      hours.toFixed(0).padStart(2, "0") +
+      ":" +
+      minutes.toFixed(0).padStart(2, "0") +
+      ":" +
+      seconds.toFixed(0).padStart(2, "0");
 
     // Happy New Year!
     if (Date.now() >= NEW_YEAR) {
-      clearInterval(timer)
+      clearInterval(timer);
 
-      itsNewYear()
+      itsNewYear();
 
-      return
+      return;
     } else if (secs <= 60) {
-      document.querySelector('#countdown').className = 'critical'
+      document.querySelector("#countdown").className = "critical";
     } else {
-      document.querySelector('#countdown').removeAttribute = 'critical'
+      document.querySelector("#countdown").removeAttribute = "critical";
     }
-  }, 1000)
+  }, 1000);
 
-  var controller = new Controller()
-  controller.init()
-  requestAnimationFrame(controller.animation)
-})
+  var controller = new Controller();
+  controller.init();
+  requestAnimationFrame(controller.animation);
+});
